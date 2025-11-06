1f0c44b30f53bc5f8b070fa9bc8ea1bb58d53682
Geethma Munasinghe <geethma_2020698@fot.ruh.ac.lk>
2025-11-06 22:11:18 +0530

    databse

---
+++ lms/src/main/java/com/pcl/lms/DB/Database.java
@@ -0,0 +1,10 @@
+package com.pcl.lms.DB;
+
+import com.pcl.lms.model.User;
+
+import java.util.ArrayList;
+
+public class Database {
+    public static ArrayList<User> userTable=new ArrayList<>();
+
+}
--- lms/src/main/java/com/pcl/lms/controller/SignUpFormController.java
+++ lms/src/main/java/com/pcl/lms/controller/SignUpFormController.java
@@ -1,9 +1,14 @@
 package com.pcl.lms.controller;
 
+import com.pcl.lms.DB.Database;
+import com.pcl.lms.model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -13,13 +18,32 @@ public class SignUpFormController {
     public AnchorPane context;
     public Label lblCompany;
     public Label lblVersion;
+    public TextField txtFullName;
+    public TextField txtAge;
+    public TextField txtEmail;
+    public PasswordField txtPassword;
 
     public void alreadyHaveAnAccountOnAction(ActionEvent actionEvent) throws IOException {
         setUI("LoginForm");
     }
 
     public void signupOnAction(ActionEvent actionEvent) throws IOException {
+        String email=txtEmail.getText();
+        String fullname=txtFullName.getText();
+        int age= Integer.parseInt(txtAge.getText());
+        String password=txtPassword.getText();
+
+        boolean emailExists= Database.userTable.stream().anyMatch(user -> user.getEmail().equals(email));
+        if (emailExists){
+            new Alert(Alert.AlertType.ERROR,"Email already exists").show();
+        }
+        User user=new User(fullname,email,age,password);
+        Database.userTable.add(user);
+        System.out.println(user.toString());
+
+        new Alert(Alert.AlertType.INFORMATION,"Account Created").show();
         setUI("LoginForm");
+
     }
     private void setUI(String location) throws IOException {
         Stage stage=(Stage) context.getScene().getWindow();
