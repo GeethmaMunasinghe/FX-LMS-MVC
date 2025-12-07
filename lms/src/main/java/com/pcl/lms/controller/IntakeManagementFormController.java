b0ff2fdcc11bd72afd7db0d624e03cee96bbd69d
Geethma Munasinghe <geethma_2020698@fot.ruh.ac.lk>
2025-12-07 22:30:08 +0530

    intake management UI

---
+++ lms/src/main/java/com/pcl/lms/controller/IntakeManagementFormController.java
@@ -0,0 +1,31 @@
+package com.pcl.lms.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+
+public class IntakeManagementFormController {
+
+    public AnchorPane context;
+    public TextField txtId;
+    public Button btnSave;
+    public DatePicker dteStart;
+    public TextField txtName;
+    public ComboBox cbxProgram;
+    public TextField txtSearch;
+    public TableView tblIntake;
+    public TableColumn colID;
+    public TableColumn colName;
+    public TableColumn colDate;
+    public TableColumn colProgram;
+    public TableColumn colOption;
+
+    public void newIntakeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void backToHomeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void saveOnAction(ActionEvent actionEvent) {
+    }
+}
